7f851c0a58c08d154a88b99f9366a53012cd9afa
Urja <urja@Urjas-MacBook-Air.local>
2024-03-29 09:09:27 +0530

    react 18 stuff

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+  "h1",
+  { id: "h1", x: "test" },
+  "Hello My React APP"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
