5cce1ac00001b8466b3e5ec46403bcdc5afa0a50
Urja <urja@Urjas-MacBook-Air.local>
2024-08-14 19:40:15 +0530

    calling functiional component in diff way

--- App.js
+++ App.js
@@ -9,9 +9,15 @@ const Title = () => {
   return <h1>I am title</h1>;
 };
 
+const title = <h1>I am title</h1>;
+
+
 //Component composition
 const Header = () => {
   return <div>
+    title
+    {title}
+    {Title()}
     <Title />
     <h1>I am Header</h1>
   </div>
