2f94efd041af1471ba841f59a6252268b27b0051
Urja <urja@Urjas-MacBook-Air.local>
2024-08-19 01:02:13 +0530

    feat: use effect and shimmer UI

--- components/Container.js
+++ components/Container.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { RestoCard } from "./RestoCard";
 import { data } from "../utils/mockdata";
+import { Shimmer } from "./Shimmer";
 
 export const RestoContainer = () => {
-  let [restoList, setRestoList] = useState(data);
+  let [restoList, setRestoList] = useState([]);
 
   handleOnClick = () => {
     const filteredList = restoList.filter((resto) => resto.info.avgRating >= 4);
     setRestoList(filteredList);
   };
+
+  fetchData = async () =>{
+    const res = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json = await res.json()
+    console.log(json)
+    setRestoList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  if(restoList?.length === 0) return <Shimmer/>
 
   return (
     <div>
@@ -18,7 +32,7 @@ export const RestoContainer = () => {
       </div>
       <div className="container">
         {restoList.map((ele) => {
-          return <RestoCard key={ele?.info?.id} restoData={ele.info} />;
+          return <RestoCard key={ele?.info?.id} restoData={ele?.info} />;
         })}
       </div>
     </div>
