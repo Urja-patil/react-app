c2bb90c8e00255cc5f043e34a813883d5d614200
Urja <urja@Urjas-MacBook-Air.local>
2024-08-24 22:34:13 +0530

    bypass cors

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 export const imgUri = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const logoUri = "https://www.uidownload.com/files/909/69/136/organic-food-restaurant-logo-template-free-vector.jpg"
-export const apiUri = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const apiUri = "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
